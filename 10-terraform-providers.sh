e1c5b79198368a8314950684df789f61f414418d
nhsy <46715264+nhsy@users.noreply.github.com>
2020-11-29 00:14:24 +0000

    Update 10-terraform-providers.sh

--- 10-terraform-providers.sh
+++ 10-terraform-providers.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-for version in 2.67.0 2.68.0 3.16.0
+for version in 2.67.0 2.68.0 3.18.0
 do
   echo "Downloading aws provider version: $version"
   wget -q https://releases.hashicorp.com/terraform-provider-aws/${version}/terraform-provider-aws_${version}_linux_amd64.zip
